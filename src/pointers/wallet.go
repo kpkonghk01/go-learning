e7910ed35c87af49ea2ee1229e79f1679d058768
Pan Kong <kpkong@hk01.com>
2023-07-22 05:03:56 +0800

    refactor: some naming

---
+++ src/pointers/wallet.go
@@ -0,0 +1,14 @@
+package pointers
+
+type Wallet struct{}
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return 0
+}
+
+// WIP, continue here:
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors#write-enough-code-to-make-it-pass
