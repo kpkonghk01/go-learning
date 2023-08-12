d9ac0918e0c88f0352d4622d9c78716ecabf221c
Pan Kong <empty@github.com>
2023-08-12 18:11:00 +0800

    feat: reflection

--- src/racer/v2/racer.go
+++ src/racer/v2/racer.go
@@ -1,13 +1,25 @@
 package racer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func Racer(a, b string) (winner string, err error) {
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
@@ -21,5 +33,3 @@ func ping(url string) chan struct{} {
 
 	return ch
 }
-
-// WIP: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/select#write-the-minimal-amount-of-code-for-the-test-to-run-and-check-the-failing-test-output-1
--- src/racer/v2/racer_test.go
+++ src/racer/v2/racer_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestRacer(t *testing.T) {
+
 	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
 		slowServer := makeDelayedServer(20 * time.Millisecond)
 		fastServer := makeDelayedServer(0 * time.Millisecond)
@@ -30,14 +31,12 @@ func TestRacer(t *testing.T) {
 		}
 	})
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
